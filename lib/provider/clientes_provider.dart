652823f782cccbe7b252721f1e5a5aa4fb6e7c5e
Moisés Alvarenga <oscar-alvarenga35@fpuna.edu.py>
2023-11-20 10:25:49 -0300

    Add insertCliente and updateCliente methods to ClientesProvider

--- lib/provider/clientes_provider.dart
+++ lib/provider/clientes_provider.dart
@@ -13,6 +13,24 @@ class ClientesProvider extends StateNotifier<List<Cliente>> {
   void removeCliente(Cliente cliente) {
     state = state.where((element) => element != cliente).toList();
   }
+
+  void insertCliente(int index, Cliente cliente) {
+    //Si el index es mayor al tamaño de la lista
+    //insertar al final
+    if (index >= state.length) {
+      state = [...state, cliente];
+      return;
+    }
+
+    state = [...state.sublist(0, index), cliente, ...state.sublist(index)];
+  }
+
+  void updateCliente(Cliente cliente) {
+    state = [
+      for (final item in state)
+        if (item.id == cliente.id) cliente else item
+    ];
+  }
 }
 
 final clientesProvider = StateNotifierProvider<ClientesProvider, List<Cliente>>(
